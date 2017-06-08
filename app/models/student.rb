1a7d5f9eedc02bd754f38ae2f487ebd020f27537
adkadziadka <industrialdog@gmail.com>
2017-06-08 23:10:28 +0200

    admin factoryGirl and super_user feature

--- app/models/student.rb
+++ app/models/student.rb
@@ -8,4 +8,8 @@ class Student < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { maximum: 50}
   validates :password, presence: true, length: { minimum: 6}
 
+  def change_status
+  	update_columns(admin: admin ? false : true)
+  end
+
  end
