7cc69dad6b9db6b990815bb82a335c60514eaf4d
OmarNizam <eng.omarnizam@gmail.com>
2017-06-08 22:46:47 +0200

    upgreat the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,15 @@ Admin.delete_all
 
 Student.delete_all
 
-
+# Admins
 admin = Admin.create!(email: 'admin@admin.com', password: '1234qwerty')
 
-student1 = Student.create!(email: 'student@student.com', password: '1234qwerty', name: 'Omar')
+# students
+student1 = Student.create!(email: 'omar@student.com', password: '1234qwerty', name: 'Omar')
 student2 = Student.create!(email: 'adrianna@student.com', password: '1234qwerty', name: 'Adrianna')
+student3 = Student.create!(email: 'maressa@student.com', password: '1234qwerty', name: 'Maressa')
+student4 = Student.create!(email: 'andrei@student.com', password: '1234qwerty', name: 'Andrei')
+student5 = Student.create!(email: 'ann@student.com', password: '1234qwerty', name: 'Ann')
+student6 = Student.create!(email: 'oliver@student.com', password: '1234qwerty', name: 'Oliver')
+student7 = Student.create!(email: 'kate@student.com', password: '1234qwerty', name: 'Kate')
+student8 = Student.create!(email: 'elizabeth@student.com', password: '1234qwerty', name: 'Elizabeth')
